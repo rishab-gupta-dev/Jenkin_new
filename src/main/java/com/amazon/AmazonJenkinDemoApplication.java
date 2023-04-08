cac6959965c42f00cc7bc33930d7ef4a94a50cda
Rishab Gupta <rggupta208@gmail.com>
2023-04-08 14:31:29 +0530

    adding jenkins proj

---
+++ src/main/java/com/amazon/AmazonJenkinDemoApplication.java
@@ -0,0 +1,22 @@
+package com.amazon;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.*;
+@SpringBootApplication
+public class AmazonJenkinDemoApplication implements CommandLineRunner {
+	public static final Logger Logger= LoggerFactory.getLogger(AmazonJenkinDemoApplication.class);
+
+	public static void main(String[] args) {
+		Logger.info("its a CI pipeline job");
+		SpringApplication.run(AmazonJenkinDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Logger.info("second log");
+		
+	}
+
+}
